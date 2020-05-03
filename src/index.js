3cfcbef640081b7f8bf685789718971459ec35b3
WellsSA <wel.cavzod@gmail.com>
2020-05-03 18:00:08 -0300

    Setting up server configuration

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+const port = 3333;
+
+app.use(routes);
+
+app.listen(port, console.info(`server running on port ${port}...`));
