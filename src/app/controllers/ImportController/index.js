c152b419cb7384026cabd9537fbf98fe2b16e282
WellsSA <wel.cavzod@gmail.com>
2020-05-12 15:58:43 -0300

    feature: ImportController req.file verification

--- src/app/controllers/ImportController/index.js
+++ src/app/controllers/ImportController/index.js
@@ -8,13 +8,14 @@ const IMPORT_SCHEMA = {
 class ImportController {
   async store(req, res) {
     if (!(await validateSchema(req.params, IMPORT_SCHEMA, res))) return;
+    if (!req.file) return res.status(400).json({ error: 'File not found' });
 
     switch (req.params.kind) {
       case 'professores':
-        // console.log('req.params.kind:', req.params.kind);
+        console.log('req.params.kind:', req.params.kind);
         return res.json(req.file);
       default:
-        return res.status(400).json('Invalid kind');
+        return res.status(400).json({ error: 'Invalid kind' });
     }
   }
 }
