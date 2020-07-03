bf4d8ce9581b4497d12d685d1af489146a582227
WellsSA <wel.cavzod@gmail.com>
2020-07-03 06:14:44 -0300

    feature(): implementing aniversariantes filter

--- src/app/controllers/FiltersController/aniversariantes.filter.js
+++ src/app/controllers/FiltersController/aniversariantes.filter.js
@@ -1,11 +1,20 @@
+import { Op, where, fn, col } from 'sequelize';
 import { Aluno } from '../../models';
 import { EXPECTED_ALUNO } from '../../utils/response';
 
-export default async (ids, userId) => {
+export default async (_, userId) => {
+  const today = new Date();
   return Aluno.findAll({
     where: {
-      // TODO: GET BY BIRTHDATE
-      id_escola: userId,
+      [Op.and]: [
+        { id_escola: userId },
+        where(fn('DAY', col('data_nascimento')), {
+          [Op.eq]: today.getDate(),
+        }),
+        where(fn('MONTH', col('data_nascimento')), {
+          [Op.eq]: today.getMonth() + 1,
+        }),
+      ],
     },
     ...EXPECTED_ALUNO,
   });
