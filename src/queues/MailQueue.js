31b9af345f6666f1be1c56e936e1ee8f100db4ed
WellsSA <wel.cavzod@gmail.com>
2020-07-14 19:15:27 -0300

    feat(): refactoring MailQueue

--- src/queues/MailQueue.js
+++ src/queues/MailQueue.js
@@ -3,12 +3,20 @@ import redisConfig from '../config/redis';
 
 export const MailQueueKey = 'mail';
 
-const MailQueue = new Queue(MailQueueKey, {
-  limiter: {
-    max: 90, // FIXME: get AWS Quota
-    duration: 1000,
-  },
-  redis: redisConfig,
-});
+class MailQueue {
+  constructor() {
+    try {
+      this.queue = new Queue(MailQueueKey, {
+        limiter: {
+          max: 90, // FIXME: get AWS Quota
+          duration: 1000,
+        },
+        redis: redisConfig,
+      });
+    } catch (err) {
+      console.error('@MailQueue/constructor. Error:', err.message);
+    }
+  }
+}
 
-export default MailQueue;
+export default new MailQueue().queue;
